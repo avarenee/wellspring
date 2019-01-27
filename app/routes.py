d4b1e26d5723127f7974d34f2ba9668b1b049663
Adam M. Rahman <mrahmanadam@gmail.com>
2019-01-26 20:10:52 -0500

    Transfer from working directory; Flask setup

---
+++ app/routes.py
@@ -0,0 +1,10 @@
+from flask import render_template
+from app import app
+
+@app.route('/')
+@app.route('/index')
+def index():
+	user = {
+		'username': 'Ada'
+		}
+	return render_template("index.html", title = "Wellspring", user = user)
